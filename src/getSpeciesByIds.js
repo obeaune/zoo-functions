f2768752e92645115f57489d7850e3fbc717d051
Obeaune <linecora86@gmail.com>
2022-01-14 18:05:31 -0300

    Testando o Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-
+function getSpeciesByIds(...ids) {
+  const species = [];
+  // verificar se o(s) ID(s) existe(m) e adicioná-lo(s) no array criado anteriormente;
+  ids.forEach((id) => species.push(data.species.find((specie) => specie.id === id)));
+  return species;
 }
 
 module.exports = getSpeciesByIds;
