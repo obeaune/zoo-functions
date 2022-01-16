59b67220a52e661c19f07f8cb9a244e751a5c2b7
Obeaune <linecora86@gmail.com>
2022-01-16 17:40:02 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
+const allAnimals = () => {
+  const objFinal = {};
+  data.species.forEach((animals) => {
+    objFinal[animals.name] = animals.residents.length;
+  });
+  return objFinal;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) return allAnimals();
+
+  const specific = data.species.find((animals) => (animals.name === animal.specie)).residents;
+  if (Object.keys(animal).length === 2) {
+    return specific.filter((anArr) => anArr.sex === animal.sex).length;
+  }
+  return specific.length;
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
