c12d1bfaadaccae2330a0b8f097a3e9e91d94a6a
Obeaune <linecora86@gmail.com>
2022-01-17 13:05:55 -0300

    Requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,33 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeesEspecific(busca) {
+  const buscaFunci = employees.find((funcionario) =>
+    (funcionario.firstName === busca.name)
+    || (funcionario.lastName === busca.name)
+    || (funcionario.id === busca.id));
+  const informacoes = {
+    id: buscaFunci.id,
+    fullName: `${buscaFunci.firstName} ${buscaFunci.lastName}`,
+    species: buscaFunci.responsibleFor.map((animalId) =>
+      species.find((specie) => specie.id === animalId).name),
+    locations: buscaFunci.responsibleFor.map((animalId) =>
+      species.find((specie) => specie.id === animalId).location),
+  };
+  return informacoes;
+}
+
+function getEmployeesCoverage(buscaGeral) {
+  if (!buscaGeral) {
+    const lista = [];
+    employees.forEach((func) =>
+      lista.push(getEmployeesEspecific({ name: func.firstName })));
+    return lista;
+  }
+  try {
+    return getEmployeesEspecific(buscaGeral);
+  } catch (error) {
+    throw new Error('Informações inválidas');
+  }
 }
 
 module.exports = getEmployeesCoverage;
