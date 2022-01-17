cfabd325f7a040e2ea929b826515184f854fb605
Obeaune <linecora86@gmail.com>
2022-01-17 11:55:07 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
+// const p = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((pessoa) => pessoa.age < 18);
+  const adult = entrants.filter((pessoa) => pessoa.age >= 18 && pessoa.age < 50);
+  const senior = entrants.filter((pessoa) => pessoa.age >= 50);
+  return ({ adult: adult.length, child: child.length, senior: senior.length });
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants = {}) {
+  if (Object.keys(entrants).length === 0) return 0;
+
+  const person = countEntrants(entrants);
+  return (person.adult * data.prices.adult) + (person.child * data.prices.child)
+  + (person.senior * data.prices.senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
